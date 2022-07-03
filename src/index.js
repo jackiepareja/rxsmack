83b889ca763ce6cb8c420320d27d37b83192ae64
jackiepareja <pareja.jackie@gmail.com>
2022-07-03 10:16:18 -0700

    Update all pre-existing libraries to most-up-to-date versions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './styles/css/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
